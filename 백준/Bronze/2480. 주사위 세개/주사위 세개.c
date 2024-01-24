928a6e713207e8e1938bb5a4a8b52063aba68b1a
Danto <74861101+Danto7632@users.noreply.github.com>
2024-01-25 00:43:18 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a==b&&b==c)
+        printf("%d", 10000+a*1000);
+    else if (a==b||b==c)
+        printf("%d", 1000+b*100);
+    else if (a==c)
+        printf("%d", 1000+a*100);
+    else
+        printf("%d", (a>b?a>c?a:c:b>c?b:c)*100);
+}
