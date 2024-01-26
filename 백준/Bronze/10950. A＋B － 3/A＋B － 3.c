368a9eb34c1b44c15d85b9bdf4cb954a8d90f296
Danto <74861101+Danto7632@users.noreply.github.com>
2024-01-26 23:15:29 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int n,a,b;
+    scanf("%d", &n);
+    
+    for (int i=0;i<n;i++) {
+        scanf("%d %d\n", &a, &b);
+        printf("%d\n", a+b);
+    }
+}
