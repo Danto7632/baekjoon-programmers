095b0abc2f045296d4012b5de95ebd37da20d317
Danto <74861101+Danto7632@users.noreply.github.com>
2024-05-10 12:25:22 +0900

    [Bronze II] Title: 커트라인, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, numa[1000], numb[1000], max=0, sum=0, count=0;
+    scanf("%d %d", &n, &k);
+    
+    for (int i=0;i<n;i++){
+        scanf("%d", &numa[i]);
+        sum += numa[i];
+        if (max < numa[i]){
+            max = numa[i];
+        }
+    }
+
+    for (int i=0;i<=max;i++){
+        for (int j=0;j<n;j++){
+            if (i==numa[j]){
+                numb[count]=i;
+                count++;
+            }
+        }
+    }
+    
+    printf("%d", numb[n-k]);
+}
