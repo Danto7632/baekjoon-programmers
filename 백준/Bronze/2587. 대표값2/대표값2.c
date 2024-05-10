d0c1b65167fea57d75da8df2aed53fe249fce93c
Danto <74861101+Danto7632@users.noreply.github.com>
2024-05-10 12:23:06 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.c
+++ 백준/Bronze/2587. 대표값2/대표값2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int numa[1000], numb[1000], mid=0, sum=0, count=0;
+    int numa[1000], numb[1000], max=0, sum=0, count=0;
 
     for (int i=0;i<5;i++){
         scanf("%d", &numa[i]);
         sum += numa[i];
-        if (mid < numa[i]){
-            mid = numa[i];
+        if (max < numa[i]){
+            max = numa[i];
         }
     }
 
-    for (int i=10;i<=mid;i+=10){
+    for (int i=10;i<=max;i+=10){
         for (int j=0;j<5;j++){
             if (i==numa[j]){
                 numb[count]=i;
