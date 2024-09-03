f189c78ddba7c3ddec78519e959fe9ad45a35d20
Danto <74861101+Danto7632@users.noreply.github.com>
2024-09-03 11:03:50 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 60 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x;
+    int y;
+}coord;
+
+int compare (const void* a, const void* b)
+{
+    coord A = *(coord*)a;
+    coord B = *(coord*)b;
+    if (A.x > B.x){
+        return 1;
+    }
+    else if (A.x == B.x){
+        if (A.y > B.y){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    coord arr[n];
+
+    for (i=0;i<n;i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    
+    qsort(arr, n, sizeof(coord), compare);
+
+    // 정렬 후
+    for (i=0;i<n;i++){
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+}
+// qsort 퀵솔트 퀵정렬 빠른정렬 (내장함수 사용)
+// 구조체를 만들고 구조체로 배열선언 후 구조체로 퀵정렬
