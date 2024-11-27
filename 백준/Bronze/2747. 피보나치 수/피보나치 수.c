d33aaee41c88976137a9491e44d8eb849bb404a4
tluda <74861101+Danto7632@users.noreply.github.com>
2024-11-27 16:54:09 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.c
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-void fibo(int* a, int* b);
-
 int main() {
-    int n, a=0, b=1;
+    int n, a=0, b=1, sum;
     scanf("%d", &n);
 
     for (int i=0;i<n;i++){
-        fibo(&a, &b);
+        sum = a + b;
+        a = b;
+        b = sum;
     }
     printf("%d", a);
-}
-
-void fibo(int* a, int* b){
-    int sum = *a + *b;
-    *a = *b;
-    *b = sum;
 }
