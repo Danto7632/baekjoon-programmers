72792c5e95cc6f2db335dbda9329bc2024694711
tluda <74861101+Danto7632@users.noreply.github.com>
2025-02-22 21:19:09 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i=0;i<n/5;i++){
+        printf("V");
+    }
+    for (int i=0;i<n%5;i++){
+        printf("I");
+    }
+}
