8fa6070b0cd43c680fd3e5411da760f8387948c1
tluda <74861101+Danto7632@users.noreply.github.com>
2025-03-03 22:32:04 +0900

    [Bronze V] Title: Pyramids, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    while(1){
+        scanf("%d", &n);
+        if (n!=0){
+            printf("%d\n", n*(n+1)/2);
+        }
+        else{
+            break;
+        }
+    }
+}
