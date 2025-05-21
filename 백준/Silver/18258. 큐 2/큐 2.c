99e49d1e4a146af0b7405d7c0d3ece19d055a02e
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-05-22 08:53:03 +0900

    [Silver IV] Title: 큐 2, Time: 436 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n,front=0,back=0,que[2000001];
+    char command[6];
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        scanf("%s",command);
+        if (strcmp(command,"push")==0){
+            scanf("%d", &que[back]);
+            back++;
+        }
+        else if (strcmp(command,"pop")==0){
+            if (back-front==0){
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", que[front]);
+                front++;
+            }
+        }
+        else if (strcmp(command,"size")==0){
+            printf("%d\n", back-front);
+        }
+        else if (strcmp(command,"empty")==0){
+            printf("%d\n", back-front==0?1:0);
+        }
+        else if (strcmp(command,"front")==0){
+            printf("%d\n", back-front==0?-1:que[front]);
+        }
+        else if (strcmp(command,"back")==0){
+            printf("%d\n", back-front==0?-1:que[back-1]);
+        }
+    }
+}
