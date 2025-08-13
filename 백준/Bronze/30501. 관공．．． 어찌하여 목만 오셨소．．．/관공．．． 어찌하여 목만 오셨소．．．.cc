a25b4cafd8916ce5bf68417643dede15ced01424
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-08-13 23:42:46 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string str;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> str;
+        for (int j=0;j<str.length();j++){
+            if (str[j]=='S'){
+                cout << str;
+                i=n;
+                break;
+            }
+        }
+    }
+}
