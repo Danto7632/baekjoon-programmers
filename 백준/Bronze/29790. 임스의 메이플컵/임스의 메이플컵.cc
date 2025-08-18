eb61562cb556709ed6a3b2be765cb9ce5559f379
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-08-18 23:47:19 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a>=1000){
+        if (b>=8000||c>=260){
+            cout << "Very Good";
+        }
+        else {
+            cout << "Good";
+        }
+    }
+    else {
+        cout << "Bad";
+    }
+}
