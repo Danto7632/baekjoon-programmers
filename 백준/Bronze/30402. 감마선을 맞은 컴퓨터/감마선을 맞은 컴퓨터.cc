3fa16894c8a84237387b5340805e2c861f42327f
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-08-25 11:15:26 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char c;
+    for (int i=0;i<225;i++){
+        cin >> c;
+        if (c=='w'){
+            cout << "chunbae";
+            break;
+        }
+        else if (c=='b'){
+            cout << "nabi";
+            break;
+        }
+        else if (c=='g'){
+            cout << "yeongcheol";
+            break;
+        }
+    }
+}
