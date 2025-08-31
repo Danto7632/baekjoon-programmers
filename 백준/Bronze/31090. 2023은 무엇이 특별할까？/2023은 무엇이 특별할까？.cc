5f4254e61fa9d6bc1d300983a90f83619f4505b2
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-09-01 05:51:42 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 228 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T,N;
+    cin >> T;
+    for (int i=0;i<T;i++){
+        cin >> N;
+        if ((N+1)%(N%100)==0){
+            cout << "Good";
+        }
+        else {
+            cout << "Bye";
+        }
+        cout << "\n";
+    }
+}
