ab46a6e6ee922704a952b142b5cab37341fbd5d5
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-10-06 21:48:50 +0900

    [Bronze IV] Title: Quadrilateral, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n,x,y;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> x >> y;
+        for (int j=0;j<y;j++){
+            for (int k=0;k<x;k++){
+                cout << "X";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+}
