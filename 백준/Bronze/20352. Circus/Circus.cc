07be8a3933db38b83d3ab33ba7f38d8dfa530aaa
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-10-14 19:16:28 +0900

    [Bronze IV] Title: Circus, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double n;
+    cin >> n;
+    cout.precision(12);
+    cout << sqrt(n/3.14159265358979)*2*3.14159265358979;
+}
