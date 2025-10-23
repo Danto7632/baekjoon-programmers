ece9909b35e0f06f2e993626e9791913678da652
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-10-23 16:11:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 23192 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
     int n,b,c,count;
+    vector<vector<int>> arr(26,vector<int>(200000,-1));
     char a;
     string str;
     cin >> str;
@@ -11,11 +16,14 @@ int main() {
     for (int i=0;i<n;i++){
         count=0;
         cin >> a >> b >> c;
-        for (int j=b;j<=c;j++){
-            if (str[j]==a){
-                count++;
+        if (arr[a-97][c]==-1){
+            for (int i=0;i<=c;i++){
+                if (str[i]==a){
+                    count++;
+                }
+                arr[a-97][i]=count;
             }
         }
-        cout << count << "\n";
+        cout << arr[a-97][c]-(b==0?0:arr[a-97][b-1]) << "\n";
     }
 }
