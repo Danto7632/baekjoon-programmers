8b5397b7664a4790b92c2346e95f875f97b502cb
김인성 <74861101+Danto7632@users.noreply.github.com>
2025-11-07 17:25:07 +0900

    [Bronze IV] Title: 장기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32684. 장기/장기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a,b,c,d,e,f,sum;
+    cin >> a >> b >> c >> d >> e >> f;
+    sum=a*13+b*7+c*5+d*3+e*3+f*2;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << (sum>a*13+b*7+c*5+d*3+e*3+f*2+1.5?"cocjr0208":"ekwoo");
+}
