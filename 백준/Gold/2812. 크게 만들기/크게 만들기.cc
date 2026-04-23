e5a62951b082c2775e02611aec1587724c116035
Inseong Kim <74861101+Danto7632@users.noreply.github.com>
2026-04-23 20:21:42 +0900

    [Gold III] Title: 크게 만들기, Time: 28 ms, Memory: 3936 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.cc
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -1,12 +1,27 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    int N,K,count=0;
+    string str;
+    deque<int> dq;
+    cin >> N >> K >> str;
+    for (int i=0;i<N;i++){
+        if (dq.empty()){
+            dq.push_back(str[i]-48);
+        }
+        else if (count!=K&&dq.back()<str[i]-48){
+            i--;
+            count++;
+            dq.pop_back();
+        }
+        else {
+            dq.push_back(str[i]-48);
+        }
+    }
+    for (int i=0;i<N-K;i++){
+        cout << dq[i];
+    }
+}
