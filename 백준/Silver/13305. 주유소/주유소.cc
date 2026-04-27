335d8905eed4da3c3a9c9e0a12a1b4fdcb49851b
Inseong Kim <74861101+Danto7632@users.noreply.github.com>
2026-04-28 02:25:13 +0900

    [Silver III] Title: 주유소, Time: 52 ms, Memory: 2412 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N,a,min=1000000001,sum=0;
+    cin >> N;
+    vector<int> arr(N,0);
+    for (int i=0;i<N-1;i++){
+        cin >> arr[i];
+    }
+    for (int i=0;i<N;i++){
+        cin >> a;
+        if (a<min){
+            min=a;
+        }
+        sum+=arr[i]*min;
+    }
+    cout << sum;
+}
